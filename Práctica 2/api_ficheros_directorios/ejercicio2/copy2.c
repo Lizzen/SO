dea33b4bb0ac98b50c0c3f2a6e32eb2e04828064
DEIVISS <99610648+Lizzen@users.noreply.github.com>
2023-10-25 15:37:08 +0200

    Update copy2.c

--- Práctica 2/api_ficheros_directorios/ejercicio2/copy2.c
+++ Práctica 2/api_ficheros_directorios/ejercicio2/copy2.c
@@ -1,114 +1,110 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <unistd.h>
 #include <fcntl.h>
-#include <string.h>
 
+#define BUFSIZE 512
 void copy(int fdo, int fdd)
 {
-	int n_read;
-	char buffer[BUFSIZE];
-	while ((n_read = read(fdo, buffer, BUFSIZE)) > 0) {
-	/* Transfer data from the buffer onto the output file */
-		if (write(fdd, buffer, n_read) < n_read) {
+	int n_read; // n palabra que va a leer
+	char buffer[BUFSIZE]; // Buffer de tamaño BUFSIZE para que vaya leyendo de 512 en 512 bytes
+
+	// Bucle que escribe en cada itineración 512 bytes
+	while ((n_read = read(fdo, buffer, BUFSIZE)) > 0){
+		if (write(fdd, buffer, n_read) < n_read){
 			perror("write");
-			close(fdo); 
+			close(fdo);
 			close(fdd);
 			exit(EXIT_FAILURE);
 		}
 	}
-	if (n_read < 0) {
+
+	if (n_read < 0){
 		perror("read");
-		close(fdo); 
+		close(fdo);
 		close(fdd);
 		exit(EXIT_FAILURE);
 	}
-	close(fdo); 
+
+	close(fdo);
 	close(fdd);
 }
 
 void copy_regular(char *orig, char *dest)
 {
-	int fdo = open(orig, O_RDONLY);
+	int fdi = open(orig, O_RDONLY);
+	if (fdi < 0){
+		perror("Error al abrir el input file");
+		exit(EXIT_FAILURE);
+	}
+
+	int fdo = open(dest, O_CREAT | O_WRONLY | O_TRUNC, 0660);
 	if (fdo < 0){
-		perror("open fdo");
+		perror("Error al abrir o crear el output file");
 		exit(EXIT_FAILURE);
 	}
 
-	int fdd = open(dest, O_CREAT  | O_WRONLY | O_TRUNC, 0660);
-		if (fdd < 0){
-		perror("open fdd");
-		exit(EXIT_FAILURE);
-	}
-
-	copy (fdo, fdd);
+	copy(fdi, fdo);
 }
 
 void copy_link(char *orig, char *dest)
 {
 	struct stat st;
 
-	//Obtenerinfo del enlace simbóñico
 	if (lstat(orig, &st) == -1){
-		perror("Error al intentar obtener información sobre el enlace simbólico");
+		perror("Error al obtener el tipo de fichero del origen");
 		exit(EXIT_FAILURE);
 	}
 
-	// Comprobar si el archivo origen es un enlace simbólico
-	if (!S_ISLNK(st.st_mode)){
-		perror("El archivo no es un enlace simbólico");
-		exit(EXIT_FAILURE);
-	}
 
-	// Reservar memoria para la ruta del archivo apuntado
-	size = st.st_size +1;
+	int size = st.st_size + 1;
 	char *path = malloc(size);
-	if (path == NULL) {
-		perror("Error malloc");
+
+	ssize_t salida = readlink(orig, path, size);
+	if (salida < 0){
+		perror("readlink");
 		exit(EXIT_FAILURE);
 	}
 
-    // Leer la ruta del archivo apuntado por el enlace simbólico
-    ssize_t result = readlink(orig, path, size);
-    if (result == -1) {
-        perror("Error readlink");
-        exit(EXIT_FAILURE);
-    }
-    target_path[result] = '\0'; // Añadir manualmente el carácter null de final de cadena
+	if (symlink(path, dest) < 0){
+		perror("symlink");
+		exit(EXIT_FAILURE);
+	}
 
-    // Crear un nuevo enlace simbólico que apunte a la misma ruta
-    if (symlink(path, dest) == -1) {
-        perror("Error symlink");
-        exit(EXIT_FAILURE);
-    }
-
-    free(path);
+	free(path);
 }
 
 int main(int argc, char *argv[])
 {
-	if (argc != 3){
-		printf("Usage: %s <sinput_file> <output_file>\n", argv[0]);
+	if (argc < 3){
+		printf("Usage: <input_file> <output_file>");
+		exit(EXIT_FAILURE);	
+	}
+
+	// Comprobar el tipo de archivo origen
+	struct stat st;
+	
+	if (lstat(argv[1], &st) == -1){
+		perror("Error al obtener el tipo de fichero del origen");
 		exit(EXIT_FAILURE);
 	}
 
-    // Comprobar el tipo de archivo origen y llamar a la función correspondiente
-    struct stat st;
-    if (lstat(argv[1], &st) == -1) {
-        perror("Error al obtener información del archivo origen");
-        exit(EXIT_FAILURE);
-    }
+	// Es un archivo regular
+	if (S_ISREG(st.st_mode)){
+		copy_regular(argv[1], argv[2]);
+	}
+	// Es un enlace simbolico
+	else if (S_ISLNK(st.st_mode)) {
+		copy_link(argv[1], argv[2]);
+	}
+	// Error, no es ninguno de los dos tipos de ficheros
+	else{
+		printf("El fichero no es regular ni simbolico");
+		exit(EXIT_FAILURE);
+	}
 
-	if (S_ISREG(st.st_mode)) {
-        copy_regular(argv[1], argv[2]);
-    } else if (S_ISLNK(st.st_mode)) {
-        copy_link(argv[1], argv[2]);
-    } else {
-        fprintf(stderr, "El archivo origen no es ni un archivo regular ni un enlace simbólico\n");
-        exit(1);
-    }
-
+	
 	return 0;
 }
